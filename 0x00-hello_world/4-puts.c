f2c7995cc537f0ab073a404772b3b7799c6f63a0
mohamedmohsen <mohamedmohsenalx@gmail.com>
2023-09-11 19:19:32 -0700

    edit1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,6 @@
 /* main function */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
