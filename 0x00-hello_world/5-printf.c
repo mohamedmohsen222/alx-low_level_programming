635a139dc4331c92c8d71dce292582ce9a1d2cbd
mohamedmohsen <mohamedmohsenalx@gmail.com>
2023-09-11 19:27:39 -0700

    5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-printf "with proper grammar, but the outcome is a piece of art,\\n"
+#include <stdio.h>
+
+/**
+ * main - fun that prints a text
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
