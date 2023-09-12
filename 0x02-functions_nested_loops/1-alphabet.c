012557cc96ee520a7187d5c8be20bcd0e34456a3
mohamedmohsen <mohamedmohsenalx@gmail.com>
2023-09-12 13:18:24 -0700

    d

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * print_alphabet - Prints the lowercase alphabet followed by a new line.
+ */
+void print_alphabet(void)
+{
+    char c;
+
+    for (c = 'a'; c <= 'z'; c++)
+    {
+        _putchar(c);
+    }
+    _putchar('\n');
+}
+
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+/**
+ * print_alphabet_x10 - Prints the lowercase alphabet 10 times, followed by a new line.
+ */
+void print_alphabet_x10(void)
+{
+    char c;
+    int i;
+
+    for (i = 0; i < 10; i++)
+    {
+        for (c = 'a'; c <= 'z'; c++)
+        {
+            _putchar(c);
+        }
+        _putchar('\n');
+    }
+}
