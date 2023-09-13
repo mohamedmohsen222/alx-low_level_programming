25f9f8ba8a28e3b1a82b5929b18221b29ebad168
mohamedmohsen <mohamedmohsenalx@gmail.com>
2023-09-13 16:52:22 -0700

    fF

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,9 +11,10 @@ int print_last_digit(int n)
     int last_digit;
 
     if (n < 0)
-        n = -n; /* Convert negative number to positive */
+        n *= -1; /* Convert negative number to positive */
 
     last_digit = n % 10;
     _putchar('0' + last_digit);
+    _putchar('\n'); // Add a newline for formatting
     return (last_digit);
 }
